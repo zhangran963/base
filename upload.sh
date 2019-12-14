64fbd8872d8a6796d28f8115898fd40e7e4165dd
ran <zhangran963@gmail.com>
2019-12-14 15:17:22 +0800

    feat: 更新 IP

--- upload.sh
+++ upload.sh
@@ -8,5 +8,5 @@ echo '已打包'
 sleep 1
 
 # 上传 dist/* 到远程
-scp -r ${rootPath}/dist/* root@39.106.117.191:/home/ran/www/base/
+scp -r ${rootPath}/dist/* root@39.106.53.163:/home/ran/www/base/
 echo '已更新'
